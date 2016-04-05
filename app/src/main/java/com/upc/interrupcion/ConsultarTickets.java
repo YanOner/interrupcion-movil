4488d521d4b7fffea0752bdd00874196f70350e3
acucho <alexcucho@gmail.com>
2016-04-05 17:27:25 -0500

    Desarrollo

--- app/src/main/java/com/upc/interrupcion/ConsultarTickets.java
+++ app/src/main/java/com/upc/interrupcion/ConsultarTickets.java
@@ -1,23 +1,20 @@
 package com.upc.interrupcion;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.Toolbar;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.support.v4.app.Fragment;
 import android.view.ViewGroup;
 
 import com.upc.ingreso.R;
 
-public class ConsultarTicket extends Fragment {
+public class ConsultarTickets extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_consultar_ticket, container, false);
+        return inflater.inflate(R.layout.activity_consultar_tickets, container, false);
     }
 
 }
